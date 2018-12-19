77bf677ff88282a734a662811fb9c4d03e316490
Houdaja <houda.jaadar@lepoles.com>
2018-12-19 17:13:28 +0100

    update

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] =[
+  {id:11, name : 'Anas Yousfi', age:22 },
+  {id:12, name : 'Assa Traore', age:22 },
+  {id:13, name : 'Hakim Belkif', age:22 },
+  {id:14, name : 'Julie Buisson', age:22 },
+  {id:15, name : 'Magali Pis', age:22},
+  {id:16, name : 'Mathieu Berdah',age:22 },
+  {id:17, name : 'Mehdi Traore', age:22},
+  {id:19, name : 'Mohamed Arous', age:22 },
+  {id:20, name : 'Sonia Bougahma', age:22},
+  {id:21, name : 'Yevette Toukam', age:22 }
+];
